fba6cce6ce7c0bda9747143da21b44d116b36b26
AL-Hassan-Sameh <125729349+AL-Hassan-Sameh@users.noreply.github.com>
2024-02-10 19:45:21 +0200

    Program.cs is edited

--- ISP/ISPDemo/Program.cs
+++ ISP/ISPDemo/Program.cs
@@ -1,7 +1,4 @@
-﻿using ISPDemo.GameEntity.Collectable.Rock;
-using ISPDemo.GameEntity.Collectable.Wood;
-using ISPDemo.GameEntity.Player;
-using System;
+﻿using System;
 
 namespace ISPDemo
 {
@@ -9,24 +6,11 @@ namespace ISPDemo
     {
         static void Main(string[] args)
         {
-            // Create game world    
+            // Create game world
             GameWorld gameWorld = new GameWorld();
 
-            // Test collecting wood
-            Console.WriteLine("\n--- Collecting Wood ---");
-            Wood wood = new Wood();
-            Player player = new Player();
-            player.Collect(wood);
-
-            // Test collecting rock
-            Console.WriteLine("\n--- Collecting Rock ---");
-            Rock rock = new Rock();
-            player.Collect(rock);
-
-            // Test player attacking and running
-            Console.WriteLine("\n--- Player Actions ---");
-            player.Attack();
-            player.Run();
+            // Run the game
+            gameWorld.Run();
         }
     }
 }
